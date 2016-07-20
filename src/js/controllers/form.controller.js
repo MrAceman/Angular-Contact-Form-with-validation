30174fe1690745996361dec4be4bb6aec0652939
Niclas Willems <niclas.willems@gmail.com>
2016-07-20 14:54:45 -0400

    Functionally largely complete. Need to add validation.

--- src/js/controllers/form.controller.js
+++ src/js/controllers/form.controller.js
@@ -1,6 +1,37 @@
 function FormController ($scope, SERVER, $http) {
 
+  $scope.formArray = [];
 
+  init();
+
+  function init () {
+    $http.get(SERVER.URL).then( (res) => {
+      $scope.formArray = res.data;
+    });
+  }
+// *******************************
+   
+    $scope.checkForWeb = (event)=>{
+      var pattern = /^https?:\/\//i;
+    }
+
+
+  $scope.submitForm = (form) => {
+    // Post takes 3 parameters. Where posting to/What you are posting
+    $http.post(SERVER.URL, form).then( (res) => {
+      // Adds form to form array
+      $scope.formArray.push(res.data);
+      $scope.form = {};
+    });
+  }
+
+  $scope.deleteMe = (id) => {
+    $http.delete(SERVER.URL + id).then((res) => {
+      $scope.formArray = $scope.formArray.filter( (form) => {
+        return form._id !== id;
+      });
+    });
+  }
 
 }
 
